3d67353382498c3d351a8a657f6b451959934451
Artem Liubatskyi <deadly241@gmail.com>
2022-12-02 07:36:49 +0200

    Initial structure

---
+++ SortAlgorithms/SortAlgorithms/Implementations/BucketSort.cs
@@ -0,0 +1,14 @@
+﻿using SortAlgorithms.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace SortAlgorithms.Implementations
+{
+    public class BucketSort : ISort
+    {
+        public IEnumerable<T> Sort<T>(T source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SortAlgorithms/SortAlgorithms/Interfaces/ISort.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SortAlgorithms.Interfaces
+{
+    public interface ISort
+    {
+        public IEnumerable<T> Sort<T>(T source);
+    }
+}
