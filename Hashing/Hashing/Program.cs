07bb1ba088b54b33d17afd6bf088b3d70764435f
Artem <44951478+artem-liubatskyi@users.noreply.github.com>
2022-12-29 07:48:08 +0200

    Feature/hashing (#5)
    * Completed implementation
    
    * Cleanup

---
+++ Hashing/Hashing/Program.cs
@@ -0,0 +1,38 @@
+﻿using Hashing.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashing
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var hashTable = new HashTable<string>(29);
+
+            foreach (var student in TestData)
+            {
+                hashTable.Add(student, student);
+            }
+
+            Console.WriteLine($"\nCollision count: { hashTable.CollisionsCount }\n");
+            Console.WriteLine(hashTable.ToString());
+            Console.Write($"======== Key: Search result ======== \n");
+
+            TestData.AddRange(new string[] { "Missing", "Test" });
+
+            foreach (var key in TestData)
+            {
+                var (value, collisions) = hashTable.Get(key);
+
+                Console.WriteLine($"{ key }: Value => { value ?? "not found"} - Collisions => {collisions}");
+            }
+            Console.ReadKey();
+        }
+
+        private static readonly List<string> TestData = new List<string>() { };
+    }
+}
